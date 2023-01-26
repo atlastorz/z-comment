b61a0e0cab7f3bf7fa13f28e238ccd732e55b51f
atlastorz <93272333+atlastorz@users.noreply.github.com>
2023-01-27 01:03:08 +0800

    Update next.config.js
    feat: add cors header to all js file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/js/:filename(.+\.js)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
   rewrites() {
     return [
       {
